454e42f5ab6f41cd1b0292794f77cf8624cae761
Lily Cheng <lilychxng@gmail.com>
2022-02-21 17:23:58 -0500

    camera movement

--- Assets/Scripts/CameraClamp.cs
+++ Assets/Scripts/CameraClamp.cs
@@ -5,15 +5,31 @@ using UnityEngine;
 public class CameraClamp : MonoBehaviour
 {
     private Quaternion myRotation;
+    //public static GameObject followObject = GameObject.Find("Player");
+    //public Rigidbody2D rbFollow = followObject.GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
 
     private void Start()
     {
         myRotation = this.transform.rotation;
-    }
+        //rbFollow = followObject.GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
 
-    void Update()
+}
+
+void Update()
     {
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -2, 100), Mathf.Clamp(transform.position.y, 0, 0), transform.position.z);
+        var followObject = GameObject.Find("Player");
+
+        if (followObject.transform.position.y > 1)
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -2, 100), Mathf.Clamp(transform.position.y, followObject.transform.position.y, 100), transform.position.z);
+            Debug.Log("Higher Up");
+        }
+        else
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -2, 100), Mathf.Clamp(transform.position.y, 0, 0), transform.position.z);
+            Debug.Log("On Ground");
+        }
+
         this.transform.rotation = myRotation;
 
     }
